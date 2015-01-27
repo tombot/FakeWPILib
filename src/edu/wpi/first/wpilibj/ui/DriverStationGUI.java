9ca66acf64f26708ca39d2ed90f3ff264903a07f
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-01-26 22:22:03 -0800

    Add gui for enable

---
+++ src/edu/wpi/first/wpilibj/ui/DriverStationGUI.java
@@ -0,0 +1,99 @@
+package edu.wpi.first.wpilibj.ui;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class DriverStationGUI extends javax.swing.JFrame {
+	
+	DriverStation m_ds;
+	
+	/** Creates new form CelsiusConverterGUI */
+	public DriverStationGUI(DriverStation ds) {
+		m_ds = ds;
+		initComponents();
+	}
+	
+	public String modeText() {
+		System.out.println("thing: " + m_ds);
+		return m_ds == null ? "Not connected"  : m_ds.isDisabled() ? "Disabled" : m_ds.isAutonomous() ? "Autonomous" : "Teleop";
+	}
+
+	// desc=" Generated Code ">//GEN-BEGIN:initComponents
+	private void initComponents() {
+		disableButton = new javax.swing.JButton();
+		teleopButton = new javax.swing.JButton();
+		autoButton = new javax.swing.JButton();
+		
+		modeLabel = new javax.swing.JLabel();
+
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setTitle("Driver Station");
+
+
+		disableButton.setText("Disable");
+		teleopButton.setText("Tele-Op");
+		autoButton.setText("Autonomous");
+		
+		disableButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				m_ds.setDSMode(true, false);
+			}
+		});
+		
+		teleopButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				m_ds.setDSMode(false, false);
+			}
+		});
+		
+		autoButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				m_ds.setDSMode(false, true);
+			}
+		});
+
+		modeLabel.setText(modeText());
+		
+		
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+				getContentPane());
+		
+		getContentPane().setLayout(layout);
+		
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		
+		
+		
+		
+		layout.setHorizontalGroup(layout.createParallelGroup()
+				.addComponent(modeLabel)
+				.addComponent(disableButton)
+				.addComponent(teleopButton)
+				.addComponent(autoButton));
+		layout.setVerticalGroup(layout.createSequentialGroup()
+				.addComponent(modeLabel)
+				.addComponent(disableButton)
+				.addComponent(teleopButton)
+				.addComponent(autoButton)
+		);
+		layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] { autoButton });
+		layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] { autoButton });
+		pack();
+	}
+
+	public void start() {
+		final DriverStationGUI _this = this;
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				_this.setVisible(true);
+			}
+		});
+	}
+
+	private javax.swing.JLabel modeLabel;
+	private javax.swing.JButton disableButton;
+	private javax.swing.JButton teleopButton;
+	private javax.swing.JButton autoButton;
+
+}
