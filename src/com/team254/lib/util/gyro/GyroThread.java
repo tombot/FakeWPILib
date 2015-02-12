216bed214f08004d968ab96239dd35c2cd4e6a10
Leigh Pauls <leighpauls@fb.com>
2015-02-11 23:03:24 -0800

    Shim for the gyro thread

---
+++ src/com/team254/lib/util/gyro/GyroThread.java
@@ -0,0 +1,21 @@
+package com.team254.lib.util.gyro;
+
+/**
+ * Fake implementation of the gyro for the simulation shim
+ * The class file of this version overwrites the class file of the real implementation when
+ * copied over to the simulation binary directory.
+ */
+public class GyroThread {
+    public void start() {}
+
+    public boolean hasData() {
+        return true;
+    }
+
+    public double getAngle() {
+        return 0;
+    }
+
+    public void rezero() {}
+
+}
